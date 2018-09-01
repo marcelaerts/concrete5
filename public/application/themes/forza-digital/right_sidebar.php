bb95676a40979a493d8df853f55ca18b7b3f6c38
Marcel Aerts <marcelaerts@kpnmail.nl>
2018-09-01 14:47:11 +0200

    Added right_sidebar.php.

---
+++ public/application/themes/forza-digital/right_sidebar.php
@@ -0,0 +1,54 @@
+<?php defined('C5_EXECUTE') or die("Access Denied.");?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php View::element('header_required'); ?>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="<?=$view->getThemePath()?>/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=$view->getThemePath()?>/css/carousel.css" rel="stylesheet">
+    <link href="<?=$view->getThemePath()?>/css/admin-fixes.css" rel="stylesheet">
+
+    <!-- Test with .less file -->
+    <link href="<?=$view->getStylesheet('main.less')?>" rel='stylesheet'>
+</head>
+<body>
+
+    <div class="<?php echo $c->getPageWrapperClass() ?>">
+    
+        <?php $view->inc('elements/header.php'); ?>
+
+        <main role="main">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-content">
+                        <?php
+                        $a = new Area('Main');
+                        $a->display($c);
+                        ?>
+                    </div>
+                    <div class="col-sm-offset-1 col-sm-3 col-sidebar">
+                        <?php
+                        $a = new Area('Sidebar');
+                        $a->display($c);
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+            <?php $view->inc('elements/footer.php'); ?>
+        </main>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?=$view->getThemePath()?>/js/popper.min.js"></script>
+    <script src="<?=$view->getThemePath()?>/js/bootstrap.min.js"></script>
+
+    <?php View::element('footer_required'); ?>
+</body>
+</html>
