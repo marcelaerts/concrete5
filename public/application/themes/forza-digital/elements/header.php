d7d08424fc752acae45b2d023bd93353799e4914
Marcel Aerts <marcelaerts@kpnmail.nl>
2018-09-02 12:18:34 +0200

    Cleanup header html and css.

--- public/application/themes/forza-digital/elements/header.php
+++ public/application/themes/forza-digital/elements/header.php
@@ -9,17 +9,6 @@
             $a = new GlobalArea('Header Navigation');
             $a->display();
             ?>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
         </div>
     </nav>
 </header>
