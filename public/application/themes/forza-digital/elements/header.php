840d864691eb8726c341a280e6cb0a556c822e89
Marcel Aerts <marcelaerts@kpnmail.nl>
2018-09-09 12:04:18 +0200

    Add logo to header.

--- public/application/themes/forza-digital/elements/header.php
+++ public/application/themes/forza-digital/elements/header.php
@@ -1,6 +1,8 @@
-<header class="header mb-5">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Carousel</a>
+<header class="header">
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <a class="navbar-brand" href="<?=URL::to('/')?>">
+            <img src="<?=$view->getThemePath()?>/images/logo.svg" alt="Forza Digital">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
